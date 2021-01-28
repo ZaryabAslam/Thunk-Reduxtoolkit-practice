cb3328b7a6f3025946270adb18f5a7067da94d85
Zaryab Aslam <zaryabaslam553@gmail.com>
2021-01-28 14:52:55 +0500

    using redux thunk for asycn call

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import App from './App';
 import store from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
-
+import { makeServer } from './server';
+makeServer({ environment: 'development' });
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
